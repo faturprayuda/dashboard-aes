ebf8e2edc0bc9cd6e3adca3da4c294b616f314de
Faturprayuda <faturprayuda@gmail.com>
2021-06-16 16:47:24 +0700

    init
    initi project with auth and template for dashboard admin and users

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::guest()) {
+            return redirect()->guest('login');
+        } else if (Auth::user()->role == 0) {
+            return $next($request);
+        }
+        return redirect()->back();
+    }
+}
---
+++ resources/views/dashboard/layouts/master.blade.php
@@ -0,0 +1,35 @@
+@include('dashboard.layouts.header')
+
+<body>
+    <div id="app">
+        <div id="sidebar" class="active">
+            <div class="sidebar-wrapper active">
+                <div class="sidebar-header">
+                    <div class="d-flex justify-content-between">
+                        <div class="logo">
+                            <a href="index.html"><img src="assets/images/logo/logo.png" alt="Logo" srcset=""></a>
+                        </div>
+                        <div class="toggler">
+                            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="sidebar-menu">
+                    @include('dashboard.layouts.include.navbar')
+                </div>
+                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+            </div>
+        </div>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+
+            @yield('content')
+
+            @include('dashboard.layouts.footer')
+</body>
+
+</html>
